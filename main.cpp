5f1b5f3e786e9eac7db20d47a7d4ff6315996f35
军 李 <1406939971@qq.com>
2020-07-08 13:51:54 +0800

    流迭代器

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#include <QCoreApplication>
+#include <iostream>
+#include <iterator>
+#include <fstream>
+using namespace std;
+
+//使用流迭代器读取文本
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    vector<string> vec;
+
+    QString path = a.applicationDirPath();
+    QString fileName=path+"\\test.txt";
+    QByteArray arr;
+    arr.append(fileName);
+
+    ifstream in(arr.data());
+    if(!in.is_open())
+        cout<<"file open failed"<<endl;
+
+    istream_iterator<string> int_it(in),eof;
+    while (int_it != eof) {
+        vec.push_back(*int_it);
+        int_it++;
+    }
+
+    return a.exec();
+}
