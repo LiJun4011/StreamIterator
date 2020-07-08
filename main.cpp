f851727b73587e3bfaa5acae704e5c0e1c2dd60d
军 李 <1406939971@qq.com>
2020-07-08 14:09:08 +0800

    数组指针的使用

--- main.cpp
+++ main.cpp
@@ -35,6 +35,21 @@ int main(int argc, char *argv[])
                 it++;
         }
 
+        //数组指针的使用
+            int (*p1)[10]=func(5);
+            cout<<(*p1)[1]<<endl;
+
+            int c[4] = {1,2,3,4};
+            int (*b)[4];
+            b = &c;
+            cout << (*b)[2] << endl;
+            int a1[sizeof (c)/sizeof (int)];
+            auto ret=copy(begin(c),end(c),begin(a1));
+            sort(begin(c),end(c));
+            sort(begin(c),end(c),DescendOrder);
+            sort(begin(c),end(c),[](const int &a,const int &b)->
+                 bool {return a>b;});
+
 
     return a.exec();
 }
